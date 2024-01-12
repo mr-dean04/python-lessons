abf8268ab661d7b9061899932f54d35173c5be48
mr-dean04 <deanp.agbodjan@gmail.com>
2024-01-12 15:10:19 +0000

    added date and time

---
+++ dateTime.py
@@ -0,0 +1,9 @@
+#Modules: builtin and user defined modules
+from datetime import *
+
+reg_time = date.today()
+reg_time = 2023-12-29
+
+
+date.strftime(2023-12-29, '%d %B %Y')
+ 
---
+++ using_argv.py
@@ -0,0 +1,6 @@
+from sys import argv
+
+nameOfFile, nameOfObject = argv
+
+print(nameOfFile)
+print(nameOfObject)
